97184372f42008bd925b6616d1551c20d8dd1bc2
Maddelynne McGovern <maddelynnemcgovern@gmail.com>
2026-06-03 12:43:11 -0500

    basic homepage made

--- racetracker/src/components/Home.jsx
+++ racetracker/src/components/Home.jsx
@@ -1,5 +1,19 @@
-import { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Home() {
-    return <div>Home</div>
-}a
+
+    return (
+        <div className="home-wrapper">
+
+            {/* Page Title */}
+            <div className="home-hero">
+                <h1>Race Tracker</h1>
+                <p className="subtitle">
+                    Description of the webapp.
+                </p>
+            </div>
+
+        </div>
+    )
+}
