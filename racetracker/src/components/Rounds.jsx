ee3db1f29d4fb1fdc80945839ad40714967d79c5
Maddelynne McGovern <maddelynnemcgovern@gmail.com>
2026-06-03 13:31:15 -0500

    Made rounds page

--- racetracker/src/components/Rounds.jsx
+++ racetracker/src/components/Rounds.jsx
@@ -1,8 +1,103 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import data from '../data/sampleData.json'
 
-export default function Rounds() {
+function Card({ item }) {
+    const navigate = useNavigate()
+
+    const editLink = "/rounds/edit/" + item.round
+    const deleteLink = "";
 
     return (
-        <div>Rounds</div>
+        <div className="card">
+            <div className="card-info">
+                <div className="card-title">Round {item.round}</div>
+                <div className="card-subtitle">{item.name}</div>
+                <div className="card-description">{item.results}</div>
+
+                <div className="grid2">
+                    {/* Edit Button */}
+                    {/* TODO: Make sure the link is correct & passing data */}
+                    <button
+                        className="card-btn"
+                        onClick={(e) => {
+                            e.stopPropagation()
+                            navigate(editLink)
+                        }}
+                    >
+                        Edit
+                    </button>
+
+                    {/* TODO: Make functional */}
+                    {/* Delete Button */}
+                    <button
+                        className="card-btn"
+                        onClick={(e) => {
+                            e.stopPropagation()
+                            navigate(deleteLink)
+                        }}
+                    >
+                        Delete
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default function Rounds() {
+    const navigate = useNavigate()
+    const Rounds = data?.rounds || []
+
+    return (
+        <div>
+            <h1>Rounds</h1>
+            {/* Previous Rounds */}
+            <div className="grid2">
+                {Rounds.map((item) => (
+                    <Card key={item.name + item.link} item={item} />
+                ))}
+            </div>
+            {/* Add Round */}
+            <div className="grid1">
+                <div className="card">
+                    <div className="card-info">
+                        <div className="grid3">
+                            {/* New Round */}
+                            <button
+                                className="card-btn"
+                                onClick={(e) => {
+                                    e.stopPropagation()
+                                    navigate("/rounds/new-round")
+                                }}
+                            >
+                                + New Round
+                            </button>
+                            {/* Leaderboard */}
+                            <button
+                                className="card-btn"
+                                onClick={(e) => {
+                                    e.stopPropagation()
+                                    navigate("/leaderboard")
+                                }}
+                            >
+                                Leaderboard
+                            </button>
+                            {/* End Game */}
+                            <button
+                                className="card-btn"
+                                onClick={(e) => {
+                                    e.stopPropagation()
+                                    navigate("/game-over")
+                                }}
+                            >
+                                End Game
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
